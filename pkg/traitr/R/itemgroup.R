3c7f69479b5b33452882adf420d68d7a9a873107
jverzani (John Verzani) <jverzani@gmail.com>
2010-07-14 14:12:45 +0000

    itemgroup funEnvir
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gwidgets@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/traitr/R/itemgroup.R
+++ pkg/traitr/R/itemgroup.R
@@ -431,7 +431,8 @@ anItemGroup <- function(items=list(),
   
   obj <- ItemGroup$proto(items=items,
                          name=name,
-                         ...)
+                         ...,
+                         funEnvir=FALSE)
   obj$init_model()
   return(obj)
 }
