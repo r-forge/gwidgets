fcbcb8d551989740f4a00f2f2e8d94cbfe2b101a
jverzani (John Verzani) <jverzani@gmail.com>
2010-07-24 12:36:39 +0000

    add gwebvis
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gwidgets@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/gWidgetsWWW/R/gwebvis.R
@@ -0,0 +1,80 @@
+##' interface to the webvis package
+##'
+##' @param wv If present, a webvis object
+##' @param handler Not implemented
+##' @param action Not implemented
+##' @param container A container to place graphic into
+gwebvis <- function(wv,
+                    handler=NULL, action=NULL,
+                    container=NULL, ...) {
+
+  if(!bypassRequire("webvis"))
+    return(glabel(gettext("gwebvis needs the webvis package to be installed"), cont=container))
+
+  
+  widget <- EXTComponent$new(toplevel=container$toplevel)
+  
+  class(widget) <- c("gWebvis",class(widget))
+  widget$toplevel$do_gwebvis <- TRUE
+  if(!missing(wv))
+    widget$setValue(value=wv)
+
+
+  
+  widget$ExtConstructor <- "Ext.Panel"
+  widget$ExtCfgOptions <-  function(.) {
+    out <- list()
+    out[['border']] <- FALSE
+    out$width <- 800; out$height <- 800
+    
+    out[['html']] <- String() +
+      sprintf('"<div id=\'gWidgetsWebvis_%s\'><scr" + "ipt type=\'text/javascript+protovis\'></scr" + "ipt></div>"', .$ID)
+
+    return(out)
+  }
+  ##' for initial graphic
+  widget$footer <- function(.) {
+    out <- String() +
+      .$setValueJS() 
+    return(out)
+  }
+  ##' produce javascript
+  widget$setValueJS <- function(.,...) {
+    out <- ""
+
+    if(exists("..data", envir=., inherits=FALSE)) {
+      value <- .$..data
+
+      if(!is.null(value) && is(value, "webvis")) {
+        ## clear out
+        value$render <- "vis.root.render();"
+        value <- as.character(unfold.webvis(value))
+        value <- paste(value, collapse=";")
+        value <- gsub("\\n",";",value)
+        ## call update from Element.js (html, loadScripts, callback)
+        out <- String() +
+          ## note hack to avoid nesting script tags
+          sprintf("val=\"<scr\" + \"ipt type='text/javascript+protovis'>%s</scr\"+\"ipt>\";", value) + "\n" +
+            sprintf("Ext.fly('%s').update(val,true);", .$ID) #gWidgetsWebvis_
+      }
+    }
+    return(out)
+  }
+    
+
+  ## Handler code needs to be written. This stub just ensures it isn't
+  ## written out if specified.
+  widget$writeHandlersJS <- function(., signal, handler=NULL) { return("")}
+
+    ## XXX replace when handler code added
+    ##   if(!is.null(handler)) 
+    ##     widget$addHandlerClicked(handler, action)
+    
+  
+  ## add after CSS, scripts defined
+  container$add(widget,...)
+  invisible(widget)
+  
+  }
+
+
