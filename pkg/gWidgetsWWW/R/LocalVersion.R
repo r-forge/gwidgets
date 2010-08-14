fb8099a0b7e68f8eb83eee6429e0f08652ecc7f1
jverzani (John Verzani) <jverzani@gmail.com>
2010-08-14 01:56:36 +0000

    another local test
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gwidgets@93 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gWidgetsWWW/R/LocalVersion.R
+++ pkg/gWidgetsWWW/R/LocalVersion.R
@@ -419,6 +419,10 @@ gWidgetsWWWIsLocal <- function() {
 
 ##' Called by AJAX script to assign a value
 localAssignValue <- function(id, value, sessionID) {
+  assign("id", id, envir=.GlobalEnv)
+  assign("value", value,envir=.GlobalEnv)
+
+  
   e <- getBaseObjectFromSessionID(sessionID)
   retval <- "419"                         # expectation failed
   if(is.null(e)) {
@@ -426,6 +430,7 @@ localAssignValue <- function(id, value, sessionID) {
   } else {
     out <- fromJSON(value)
     if(is.list(out)) {
+      assign("value1", out$value, envir=.GlobalEnv)
       tmp <- try(assign(id, out$value, envir=e), silent=TRUE)
       if(!inherits(tmp, "try-error"))
         retval <- "200"                   # all good
