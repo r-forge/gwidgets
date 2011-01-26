ad3d1eb93468b324d62682d98c955101b81c10d0
jverzani (John Verzani) <jverzani@gmail.com>
2011-01-26 14:30:28 +0000

    modify statusbar code to workaround GTK changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gwidgets@283 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gWidgetsRGtk2/R/gstatusbar.R
+++ pkg/gWidgetsRGtk2/R/gstatusbar.R
@@ -1,6 +1,7 @@
 ## gtkStatusBar. Use value to push message, value to pop
 setClass("gStatusbarRGtk",
          contains="gComponentRGtk",
+         representation=representation(label="GtkLabel"),
          prototype=prototype(new("gComponentRGtk"))
          )
 ## constructor
@@ -13,9 +14,19 @@ setMethod(".gstatusbar",
             
             statusbar <- gtkStatusbarNew()
             statusbar$setHasResizeGrip(TRUE)
-            statusbar$push(statusbar$getContextId("message"), text)
+            sbl <- statusbar[[1]][[1]]
+            ## use our own label, not statusbars
+            l <- gtkLabel()
+            ## l$modifyFont(pangoFontDescriptionFromString("10px"))
+            l['xalign'] <- 0.0          # not 0.5
+            statusbar[[1]]$remove(statusbar[[1]][[1]])
+            statusbar[[1]]$add(l)
+            
+            ##statusbar$push(statusbar$getContextId("message"), text)
 
             obj <- as.gWidgetsRGtk2(statusbar)
+
+            svalue(obj) <- text
             
             if (!is.null(container)) {
               if(is.logical(container) && container == TRUE)
@@ -28,6 +39,7 @@ setMethod(".gstatusbar",
 
 as.gWidgetsRGtk2.GtkStatusbar <- function(widget,...) {
   obj <- new("gStatusbarRGtk",block=widget, widget=widget,
+             label=widget[[1]][[1]],
              toolkit=guiToolkit("RGtk2"))
   return(obj)
 }
@@ -39,14 +51,25 @@ as.gWidgetsRGtk2.GtkStatusbar <- function(widget,...) {
 setMethod(".svalue",
           signature(toolkit="guiWidgetsToolkitRGtk2",obj="gStatusbarRGtk"),
           function(obj, toolkit, index=NULL, drop=NULL, ...) {
-            obj@widget$Pop(obj@widget$getContextId("message"))
+            obj@label$getLabel()
+            ## obj@widget$Pop(obj@widget$getContextId("message"))
           })
 
 ## This pushes to stack
 setReplaceMethod(".svalue",
                  signature(toolkit="guiWidgetsToolkitRGtk2",obj="gStatusbarRGtk"),
                  function(obj, toolkit, index=NULL, ..., value) {
-                   obj@widget$Push(obj@widget$getContextId("message"), value)
+                   label <- obj@label
+                   if(value == "") value <- " " # need some text to act as strut
+                   label$setText(paste(value, collapse="\n"))
+                   # obj@widget$Push(obj@widget$getContextId("message"), value)
                    return(obj)
                  })
 
+## push font down to label
+setReplaceMethod(".font",
+                 signature(toolkit="guiWidgetsToolkitRGtk2",obj="gStatusbarRGtk"),
+                 function(obj, toolkit, ..., value) {
+                   .font(obj@label, toolkit, ...) <- value
+                   return(obj)
+                 })
