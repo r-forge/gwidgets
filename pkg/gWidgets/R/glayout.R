712ef7646eda6712b92738191120289c19a09d29
jverzani (John Verzani) <jverzani@gmail.com>
2011-07-26 17:55:48 +0000

    update to gslider, gspinbutton (add seq arguments)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gwidgets@357 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gWidgets/R/glayout.R
+++ pkg/gWidgets/R/glayout.R
@@ -48,3 +48,4 @@ setMethod("[",
               return(out)
              
           })
+
--- pkg/gWidgets/R/gspinbutton.R
+++ pkg/gWidgets/R/gspinbutton.R
@@ -20,9 +20,31 @@ setClass("gSpinbutton",
 ##'
 ##' @export
 gspinbutton =function(
-  from = 0, to = 10, by = 1, value = from, digits = 0,
+  from = 0, to = 10, by = 1,
+  length.out = NULL, along.with=NULL,
+  value = from, digits = 0,
   handler = NULL, action = NULL, container = NULL, ... ,
   toolkit=guiToolkit()){
+
+  ## mostly from seq.default
+  if (!missing(along.with)) {
+    length.out <- length(along.with)
+  } else if(!missing(length.out)) {
+    len <- length(length.out)
+    if (!len) 
+      stop("argument 'length.out' must be of length 1")
+    if (len > 1L) {
+      warning("first element used of 'length.out' argument")
+      length.out <- length.out[1L]
+    }
+    length.out <- ceiling(length.out)
+  }
+  if(!is.null(length.out)) {
+    ## set up by to be for length,out
+    by <- (to - from)/(length.out[1] - 1)
+  }
+
+
   widget <- .gspinbutton (toolkit,
                           from=from, to=to, by=by, value=value, digits=digits,
                           handler=handler, action=action, container=container ,...
