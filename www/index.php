1e5df45e9c9d1805b38bddbdb084006449337790
jverzani (John Verzani) <jverzani@gmail.com>
2012-10-08 15:39:12 +0000

    update webpage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gwidgets@453 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -39,25 +39,30 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
   <li>gWidgets: an API for writing graphical user interfaces (GUIs)
   within R. The package is cross-platform (Linux/Mac/Windows) and
   cross-toolkit (RGtk2, qtbase, tcltk). An RNews article is
-  <a href=www.r-project.org/doc/Rnews/Rnews_2007-3.pdf>here</a>. The
+       <a href=http://www.r-project.org/doc/Rnews/Rnews_2007-3.pdf>here</a>. The
   gWidgets package needs a further "gWidgetsXXX" package to provide the connection
   to the graphical toolkit.
 </li>
+<li>(The gWidgets package is being replaced by gWidgets2 which is hosted on github. See the files here: <a href='https://github.com/jverzani'>https://github.com/jverzani</a>
+</li>
+
   <li>gWidgetsRGtk2: needed to use the <a href=http://www.ggobi.org/rgtk2/>RGtk2</a> package for the graphical toolkit</li>
+
   <li>gWidgetstcltk: needed to use the tcltk package for the graphical toolkit</li>
+
   <li>gWidgetsQt: needed to use the <a href=https://r-forge.r-project.org/R/?group_id=454>qtbase</a> package for the graphical toolkit</li>
+
   <li>gWidgetsrjava: needed to use the rjava package for the graphical
-  toolkit. This package is not being actively maintained.</li>
+  toolkit. <em>This package is not being maintained. For Java-based GUIs please see the deducer project.</em></li>
   <li>gWidgetsWWW: A standalone implementation of the gWidgets API
   that allows one to easily make dynamic web sites, either locally or,
-  with rapache, to be served to a remote user. </li>
+     with rapache, to be served to a remote user. See <a href='www.jstatsoft.org/v49/i10/paper'>a JSS article</a> for some details. (This package is replaced by the
+						   gWidgetsWWW2 and gWidgetsWWW2.rapache packages on the github site). 						   
+</li>
   <li>pmg: This is a GUI for R written using gWidgets, primarily aimed around its use in the
   classroom. 
 
-    Some screenshots are
-  <a href=http://www.math.csi.cuny.edu/pmg/Screenshots/>here</a>.
-
-    The package is described <a href=www.amstat.org/publications/jse/v16n1/verzani.pdf>here</a>.
+     The package is described <a href=http://www.amstat.org/publications/jse/v16n1/verzani.pdf>here</a>.
 
 </li>
   <li>traitr: an alternate interface for programming GUIs which uses
@@ -78,8 +83,13 @@ addHandlerClicked(b, handler=function(h,...) {  # add interactivity through a ha
 })
 </pre>
 
-The gWidgets vignette is a source of further examples.
-Addtionally, some more examples may be found <a href=http://www.math.csi.cuny.edu/pmg/gWidgets/Examples/>here</a>.
+     <h3>More documentation/examples</h3>
+<p>
+     The gWidgets vignette is a source of further examples. The vignette for gWidgets2 is shortened, but still has some exmaples. (See <a href='https://github.com/jverzani/gWidgets2/blob/master/vignettes/gWidgets2.pdf'>a copy here</a>.)
+
+
+<p>
+The text <em>Programming Graphical User Interfaces in R</em> has a part on gWidgets
 
 
 
